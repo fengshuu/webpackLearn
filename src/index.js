488cfe5fb429bcfc030e815f756e1fe24bc26689
shuhaofeng2 <shuhaofeng2@creditease.cn>
2021-03-09 15:23:13 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import example from './images/example.jpg'
+import esvg from './images/example.svg'
+import './styles/main.scss'
+
+
+console.log('Interesting!')
+
+// 创建没有构造函数的类属性
+class Game {
+  name = 'Violin Charades'
+}
+const myGame = new Game()
+// 创建 p 节点
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.name}.`
+
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+const app = document.querySelector('#root')
+app.append(heading, p)
